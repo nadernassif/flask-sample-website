5a4f2b0c70542378dbc051d463ab0d67011a7622
Nader Youssef <nader.youssef@eficode.com>
2022-11-02 11:35:53 +0100

    refactored some coding and added flash messages

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.secret_key = os.urandom(12)
 #hardcoded values to be replaced with db values
 user = {"username": "nader", "password": "password"}
 
-
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if(request.method == 'POST'):
@@ -18,12 +17,13 @@ def login():
         if username == user['username'] and password == user['password']:
             
             session['user'] = username
+            flash('login successful','info')
             return redirect('/dashboard')
         else:
-            flash('wrong password?')
+            flash('wrong password?','error')
+            return redirect('/login')
     
     return render_template("login.html")
-
 
 @app.route('/dashboard')
 def dashboard():
